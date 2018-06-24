1e926cbac9bfd52212d5c45d22ca6662d3cd2208
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 19:43:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(leftNumbers, 10)
  
-  if (right > 0) {
-    dodger.style.left = `${left - 1}px`
+  if (right < 0400) {
+    dodger.style.right = `${right + 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
-    moveDodgerLeft()
+    moveDodgerRight()
   }
 })
