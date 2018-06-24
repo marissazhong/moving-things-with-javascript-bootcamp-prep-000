35289379562413ebe8cbb19cf5f1798377653379
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 19:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
  
-  if (left > 0) {
+  if (right > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
